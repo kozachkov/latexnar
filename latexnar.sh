67a324e8870f0133ae3f67cc57182b2a2d6bf38f
Михаил Козачков <mchlkzch@gmail.com>
2021-01-20 11:16:04 +0300

    добавлен latexnar.sh для создания образца из текущего месторасположения

---
+++ latexnar.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+VERSION=0.01
+
+# Получим месторасположение исполняемого файла
+SRC=$(readlink ${BASH_SOURCE[0]})
+DIR="$(cd "$(dirname $SRC)" >/dev/null 2>&1 && pwd)"
+
+# Скопируем необходимые файлы шаблонов в папку из которой был вызов
+cp $DIR/Main.tex $PWD
+cp -r $DIR/sections $PWD
+
+echo "Готово"
